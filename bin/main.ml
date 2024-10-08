ee5e6e74683ec8f4da786d8a2bab6a30860d1db0
Mark Elvers <mark.elvers@tunbury.org>
2024-10-08 09:17:23 +0100

    Conflicts need to be targetted

--- bin/main.ml
+++ bin/main.ml
@@ -45,7 +45,10 @@ type pac = {
   name : string;
   base : string;
   version : string;
+  sha256 : string;
   deps : string list;
+  provides : string list;
+  conflicts : string list;
 }
 
 module List = struct
@@ -79,6 +82,13 @@ let rec process p = function
   | "%NAME%" :: name :: tl -> process { p with name = normalise [ '.' ] name } tl
   | "%BASE%" :: base :: tl -> process { p with base } tl
   | "%VERSION%" :: version :: tl -> process { p with version = normalise [ ':' ] version } tl
+  | "%SHA256SUM%" :: sha256 :: tl -> process { p with sha256 } tl
+  | "%CONFLICTS%" :: tl ->
+      let conflicts, rest = List.split_on_element "" tl in
+      process { p with conflicts } rest
+  | "%PROVIDES%" :: tl ->
+      let provides, rest = List.split_on_element "" tl in
+      process { p with provides } rest
   | "%DEPENDS%" :: tl ->
       let deps, rest = List.split_on_element "" tl in
       process { p with deps } rest
@@ -87,17 +97,24 @@ let rec process p = function
 
 let quoted_string lst = List.map (fun x -> "\"" ^ x ^ "\"") lst |> String.concat " "
 
-let string_of_version_dep s =
+let triple_of_version s =
   let name, equality, version =
     List.fold_left
-      (fun acc n ->
+      (fun (name, equality, version) n ->
         let l = String.length n in
         let i = String.strstr s n in
-        if i >= 0 then (String.sub s 0 i, String.sub s i l, String.sub s (i + l) (String.length s - l - i)) else acc)
-      (s, "", "") [ ">="; "<="; "<"; ">"; "=" ]
+        if name = None && i >= 0 then (Some (String.sub s 0 i), Some (String.sub s i l), Some (String.sub s (i + l) (String.length s - l - i)))
+        else (name, equality, version))
+      (None, None, None) [ ">="; "<="; "<"; ">"; "=" ]
   in
-  let name = normalise [ '.' ] name in
-  if equality = "" then quoted_string [ name ] else quoted_string [ name ] ^ " {" ^ equality ^ " " ^ quoted_string [ version ] ^ "}"
+  let name = normalise [ '.' ] (Option.value ~default:s name) in
+  (name, equality, version)
+
+let string_of_version v =
+  let name, equality, version = v in
+  match (equality, version) with
+  | Some equality, Some version -> quoted_string [ name ] ^ " {" ^ equality ^ " " ^ quoted_string [ version ] ^ "}"
+  | _, _ -> quoted_string [ name ]
 
 let () =
   let _ = mkdir_p out_dir in
@@ -113,21 +130,40 @@ let () =
       |> List.iter (fun folder ->
              let desc = path [ tmp_dir; repo; folder; "desc" ] in
              let content = if Sys.is_regular_file desc then read_input desc else [] in
-             let d = process { filename = ""; name = ""; base = ""; version = ""; deps = [] } content in
+             let d = process { filename = ""; name = ""; base = ""; version = ""; sha256 = ""; deps = []; provides = []; conflicts = [] } content in
              let () = Printf.printf "%s\n%s\n%s\n%s\n" d.filename d.name d.base d.version in
              let () = List.iter (Printf.printf "%s,") d.deps in
+             let () = Printf.printf "\n\n" in
              if d.name <> "opam" then
                let opam = mkdir_p (path [ out_dir; "packages"; d.name; d.name ^ "." ^ d.version ]) in
                let oc = open_out (path [ opam; "opam" ]) in
                let () = Printf.fprintf oc "opam-version: \"2.0\"\n" in
                let () = Printf.fprintf oc "build: [%s]\n" (quoted_string [ "/usr/bin/pacman"; "-U"; "--noconfirm"; d.filename ]) in
                let () = Printf.fprintf oc "remove: [%s]\n" (quoted_string [ "/usr/bin/pacman"; "-R"; d.filename ]) in
-               let () = Printf.fprintf oc "depends: [\n" in
-               let () = List.iter (fun e -> Printf.fprintf oc "  %s\n" (string_of_version_dep e)) d.deps in
-               let () = Printf.fprintf oc "]\n" in
+               let () =
+                 if List.length d.deps > 0 then
+                   let () = Printf.fprintf oc "depends: [\n" in
+                   let () = List.iter (fun e -> Printf.fprintf oc "  %s\n" (string_of_version (triple_of_version e))) d.deps in
+                   Printf.fprintf oc "]\n"
+               in
+               let () =
+                 if List.length d.conflicts > 0 then
+                   let () = Printf.fprintf oc "conflicts: [\n" in
+                   let () = List.iter (fun e -> Printf.fprintf oc "  %s\n" (string_of_version (triple_of_version e))) d.conflicts in
+                   Printf.fprintf oc "]\n"
+               in
                let () = Printf.fprintf oc "extra-source \"%s\" {\n" d.filename in
                let () = Printf.fprintf oc "  src: \"%s/%s/os/%s/%s\"\n" mirror repo arch d.filename in
+               let () = if d.sha256 <> "" then Printf.fprintf oc "  checksum: [ \"sha256=%s\" ]\n" d.sha256 in
                let () = Printf.fprintf oc "}\n" in
-               let () = Printf.printf "\n\n" in
-               close_out oc))
+               let () = close_out oc in
+               List.iter
+                 (fun p ->
+                   let name, _, version = triple_of_version p in
+                   let opam = mkdir_p (path [ out_dir; "packages"; name; name ^ "." ^ Option.value ~default:"1" version ]) in
+                   let oc = open_out (path [ opam; "opam" ]) in
+                   let () = Printf.fprintf oc "opam-version: \"2.0\"\n" in
+                   let () = Printf.fprintf oc "depends: [ %s ]\n" (string_of_version (d.name, Some "=", Some d.version)) in
+                   close_out oc)
+                 d.provides))
     repos
